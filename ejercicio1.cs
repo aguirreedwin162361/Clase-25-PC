01639be2ffb5d7a03fa73cdfdf23ea2dea1adc87
aguirreedwin162361 <aguirreedwin162361@gmail.com>
2026-03-13 11:34:05 -0600

    creación de variables

---
+++ ejercicio1.cs
@@ -0,0 +1,26 @@
+/*Sistema 1: Sistema de Registro de Usuarios
+El programa permitirá registrar usuarios en el sistema.
+Menú principal
+1. Registrar usuario
+2. Mostrar usuarios registrados
+3. Buscar usuario por nombre
+4. Salir
+Requisitos
+Al registrar usuario se debe pedir:
+• Nombre
+• Edad
+• Correo electrónico
+• Tipo de usuario
+El tipo de usuario debe seleccionarse con switch:
+1 Administrador
+2 Cliente
+3 Invitado
+Validaciones
+• Si el nombre tiene menos de 3 caracteres, mostrar error.
+• Si la edad es menor a 18, indicar que es menor de edad.
+• Si el correo no contiene “@”, mostrar advertencia.*/
+
+string nombre = "";
+int edad;
+string correo = "";
+string tipoUsuario= "";
